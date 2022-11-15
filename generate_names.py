ad7fbd8dbf7664c0cdb3f3d704a8e247492354fd
Jane Awuah <janeawuah@gmail.com>
2022-11-15 09:38:10 +0000

    added the feature to generate addresses

--- generate_names.py
+++ generate_names.py
@@ -9,13 +9,12 @@ countries = {
     "Nigeria": ["Abuja", "Lagos", "Ibadan", ""]
 }
 
-get_gender = input("What gender should this name be?, f or m: ")
-get_age = int(input("Please enter their age: "))
 
-if(get_gender == "m"):
-    get_name = male_names[random.randrange(len(male_names))]
-else:
-    get_name = female_names[random.randrange(len(female_names))]
+def get_name(gender):
+    if(gender == "m"):
+        return male_names[random.randrange(len(male_names))]
+    else:
+        return female_names[random.randrange(len(female_names))]
 
 def nearest_ten_up(get_age):
     return math.ceil(get_age/10)*10
@@ -24,9 +23,22 @@ def nearest_ten_down(get_age):
     return math.floor(get_age/10)*10
 
 # age_range = nearest_ten_up(get_age) + " - " + nearest_ten_down(get_age)
-def get_address(country_name):
-    return ""
+def get_address(country):
+    gen_city = countries[country][random.randrange(len(countries[country]))]
+    street_num = random.randint(1,200)
+    # address= random.randrange[200], " ", countries[country][random.randrange(len(country))], " Street"
+#    address = '',street_num ,'', gen_city
+    return gen_city
 
-print("Name: ", get_name )
-print("Age range: ", nearest_ten_down(get_age) , " - " , nearest_ten_up(get_age))
-print("Address: ")
+
+
+
+
+
+gender = input("What gender should this name be?, f or m: ")
+age = int(input("Please enter their age: "))
+country = input("Please enter a country: ")
+
+print("Name: ", get_name(gender) )
+print("Age range: ", nearest_ten_down(age) , " - " , nearest_ten_up(age))
+print("Address: ", random.randint(1,200), " ", get_address(country), " Street")
